ad1a39421e27d6813cdb6c5ff01b4d1b42347b36
roryjoe <roryjoem@users.noreply.github.com>
2021-03-06 08:45:24 +0000

    Updated text to yo.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey there. This is a test applcation written in Javascript.'));
+app.get('/', (req, res) => res.send('Yo. This is a test applcation written in Javascript.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
